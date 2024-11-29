f943543a3fbc61edc0678c3491129c606a3dc93f
JTC878 <jords1321@gmail.com>
2024-11-29 15:47:39 +0000

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+#define MAZE_X 50
+#define MAZE_Y 20
+
+using namespace std;
+
+char mazeArray[MAZE_Y][MAZE_X];
+void initialiseMaze();
+void printMazeArray();
+
+
+int main() {
+	initialiseMaze();
+	printMazeArray();
+}
+
+
+
+
+
+void initialiseMaze() {
+	for (int i = 0; i < MAZE_Y; i++) {
+		for (int j = 0; j < MAZE_X; j++) {
+			mazeArray[i][j] = '#';
+		}
+	}
+}
+
+void printMazeArray() {
+	for (int i = 0; i < MAZE_Y; i++) {
+		cout << endl;
+		for (int j = 0; j < MAZE_X; j++) {
+			cout << mazeArray[i][j];
+		}
+	}
+}
