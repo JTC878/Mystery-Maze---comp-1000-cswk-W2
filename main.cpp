b7aa4ed57bf6360ea791ca9049a58644f9b98ca0
JTC878 <jords1321@gmail.com>
2024-11-29 16:53:25 +0000

    random maze WIP

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <string>
+#include <random>
 
 #define MAZE_X 50
 #define MAZE_Y 20
 
+struct mazeStartingPoint {
+	int y = 1;
+	int x = 0;
+};
+
 using namespace std;
 
-char mazeArray[MAZE_Y][MAZE_X];
+char mazeArr[MAZE_Y][MAZE_X];
 void initialiseMazeArray();
 void printMazeArray();
-void initialiseRandomMaze();
+void initialiseMainPath();
 
 
 int main() {
@@ -24,7 +30,7 @@ int main() {
 void initialiseMazeArray() {
 	for (int i = 0; i < MAZE_Y; i++) {
 		for (int j = 0; j < MAZE_X; j++) {
-			mazeArray[i][j] = '#';
+			mazeArr[i][j] = '#';
 		}
 	}
 }
@@ -33,11 +39,30 @@ void printMazeArray() {
 	for (int i = 0; i < MAZE_Y; i++) {
 		cout << endl;
 		for (int j = 0; j < MAZE_X; j++) {
-			cout << mazeArray[i][j];
+			cout << mazeArr[i][j];
 		}
 	}
 }
 
-void initialiseRandomMaze() {
-
+void initialiseMainPath() {
+	bool found = false;
+	int y;
+	while (found == false) {
+		for (int i = 1; i < MAZE_Y; i++) {
+			for (int j = 0; j < MAZE_X; j++) {
+				srand(time(0));
+				y = rand();
+				if (mazeArr[i - 1][j] == ' ' && y % 10 == 0) {
+					if (i == MAZE_Y) {
+						mazeArr[i][j] == ' ';
+						found = true;
+						break;
+					}
+					else {
+						mazeArr[i][j] == ' ';
+					}
+				}
+			}
+		}
+	}
 }
