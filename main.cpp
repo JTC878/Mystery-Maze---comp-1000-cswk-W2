0cb6f3f9ae51403bbbb7617ef870ea4106622463
JTC878 <jords1321@gmail.com>
2024-12-31 13:35:54 +0000

    cleanup

--- main.cpp
+++ main.cpp
@@ -8,19 +8,6 @@ using namespace std;
 
 #include "mazeclass.h"
 
-/*
-#define MAZE_X 99 
-#define MAZE_Y 49
-
-char mazeArr[MAZE_Y][MAZE_X];
-int pathCount = 1;
-mazePoint midPoint = {(MAZE_Y / 2), (MAZE_X / 2)};
-void initialiseMazeArray();
-void printMazeArray();
-void initialiseMainPath();
-
-*/
-
 
 int main() {
 	{
@@ -37,134 +24,12 @@ int main() {
 
 
 
-/*
 
-void initialiseMazeArray() {
-	for (int i = 0; i < MAZE_Y; i++) {
-		for (int j = 0; j < MAZE_X; j++) {
-			mazeArr[i][j] = '#';
-		}
-	}
-}
 
-void printMazeArray() {
-	for (int i = 0; i < MAZE_Y; i++) {
-		cout << endl;
-		for (int j = 0; j < MAZE_X; j++) {
-			cout << mazeArr[i][j];
-		}
-	}
-}
 
-int weightedRandomPercX(int k) {
-	float PercValue;
-	PercValue = abs(k - midPoint.x);
-	PercValue = PercValue / midPoint.x;
-	PercValue = 1 - PercValue;
-	PercValue = 100 * PercValue;
-	if (PercValue < 1) PercValue = 1;
-	return int(PercValue);
-}
 
-int weightedRandomPercY(int k) {
-	float PercValue;
-	PercValue = abs(k - midPoint.y);
-	PercValue = PercValue / midPoint.y;
-	PercValue = 1 - PercValue;
-	PercValue = 100 * PercValue;
-	if (PercValue < 1) PercValue = 1;
-	return int(PercValue);
-}
 
-void initialiseMainPath() { //works to a certain extent but gets stuck because of the wall checks - need to find out how that happens. 
-	mazeArr[midPoint.y][midPoint.x] = ' ';
-	int i = midPoint.y;
-	int j = midPoint.x;
-	bool found = false;
-	int r = 0;
-	int loopCounter = 0;
-	while (found == false) {
-		r = rand() % 4;
-		switch (r) {
-		case 0: //ABOVE case - ISSUE Maze will never end at top or bottom but because of the way memory addressing works for arrays ending at the left or right side is possible 
-			if ((rand() % 101) <= weightedRandomPercY(i)) { //if left, right and up is a wall
-				if (i < 5 || loopCounter > 40) {
-					mazeArr[i - 1][j] = ' ';
-					i--;
-					loopCounter = 0;
-					pathCount++;
-				}
-				else if (mazeArr[i-1][j] == '#' && mazeArr[i - 2][j] == '#' && mazeArr[i - 3][j] == '#' && mazeArr[i - 4][j] == '#') {
-					mazeArr[i - 1][j] = ' ';
-					i--;
-					loopCounter = 0;
-					pathCount++;
-				}
-			}
-			loopCounter++;
-			break;
-		case 1: //LEFT case
-			if ((rand() % 101) <= weightedRandomPercX(j)) { //if up and down is not a path
-				if (j < 5 || loopCounter > 40) {
-					mazeArr[i][j-1] = ' ';
-					j--;
-					loopCounter = 0;
-					pathCount++;
-				}
-				else if (mazeArr[i][j-1] == '#' && mazeArr[i][j - 2] == '#' && mazeArr[i][j - 3] == '#' && mazeArr[i][j - 4] == '#') {
-					mazeArr[i][j-1] = ' ';
-					j--;
-					loopCounter = 0;
-					pathCount++;
-				}
-			}
-			loopCounter++;
-			break;
-		case 2: //BELOW case
-			if ((rand() % 101) <= weightedRandomPercY(i)) { //if left and right is not a path
-				if (i > MAZE_Y - 5 || loopCounter > 40) {
-					mazeArr[i+1][j] = ' ';
-					i++;
-					loopCounter = 0;
-					pathCount++;
-				}
-				else if (mazeArr[i+1][j] == '#' && mazeArr[i + 2][j] == '#' && mazeArr[i + 3][j] == '#' && mazeArr[i + 4][j] == '#') {
-					mazeArr[i+1][j] = ' ';
-					i++;
-					loopCounter = 0;
-					pathCount++;
-				}
-			}
-			loopCounter++;
-			break;
-		case 3: //RIGHT case
-			if ((rand() % 101) <= weightedRandomPercX(j)) { //if up and down is not a path
-				if (j > MAZE_X - 5 || loopCounter > 40) {
-					mazeArr[i][j+1] = ' ';
-					j++;
-					loopCounter = 0;
-					pathCount++;
-				}
-				else if (mazeArr[i][j+1] == '#' && mazeArr[i][j + 2] == '#' && mazeArr[i][j + 3] == '#' && mazeArr[i][j + 4] == '#') {
-					mazeArr[i][j+1] = ' ';
-					j++;
-					loopCounter = 0;
-					pathCount++;
-				}
-			}
-			loopCounter++;
-			break;
-		default:
-			cout << "Something went wrong";
-		}
-		if (i == 0 || j == 0 || i == (MAZE_Y - 1) || j == (MAZE_X - 1)) {
-			found = true;
-			mazeArr[i][j] = 'D';
-		}
-	}
-}
 
-*/
 
 
 /* MAZE GENERATION STEPS
