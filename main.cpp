b95b59186f812d4d88cd82e6d4c9e86729ab7f5a
JTC878 <jords1321@gmail.com>
2025-01-04 17:57:23 +0000

    Better depthValues array format

--- main.cpp
+++ main.cpp
@@ -11,17 +11,18 @@ using namespace std;
 
 #include "mazeclass.h"
 
-const int Maze::depthValues[10][12] = { //mazeX, mazeY, percPathsofMaze*10, enemyNumber, enemySpotDistance, enemyStep, maxSlow, maxTele, maxKill, maxSUTele, maxKeys
-	{30, 20, 4, 3, 5, 1, 6, 2, 6, 10, 1, 0},
-	{40, 20, 4, 7, 5, 2, 8, 4, 7, 0, 1, 0},
-	{50, 30, 4, 11, 5, 3, 12, 6, 10, 0, 1, 0},
-	{60, 30, 4, 17, 5, 4, 16, 8, 10, 0, 1, 0},
-	{70, 40, 4, 25, 5, 5, 20, 10, 16, 0, 1, 0},
-	{80, 40, 5, 35, 5, 6, 26, 12, 18, 1, 1, 0},
-	{90, 50, 5, 50, 5, 7, 35, 20, 30, 1, 1, 0},
-	{100, 50, 5, 100, 5, 8, 45, 35, 40, 1, 1, 0},
-	{110, 60, 5, 200, 5, 9, 55, 40, 65, 1, 1, 0},
-	{120, 60, 5, 300, 5, 10, 65, 50, 70, 1, 1, 0},
+const int Maze::depthValues[10][12] = { 
+  //mazeX  mazeY percPathsofMaze*10  enemyNumber  enemySpotDistance  enemyStep  maxSlow  maxTele  maxKill  maxSUTele  maxKeys  maxDoorCount
+	{30,     20,         4,              3,               5,			 1,        6,       2,       6,        0,        1,         0},
+	{40,     20,         4,              7,               5,			 2,        8,       4,       7,        0,        1,         0},
+	{50,     30,         4,              11,              5,			 3,        12,      6,       10,       0,        1,         0},
+	{60,     30,         4,              17,              5,			 4,        16,      8,       10,       0,        1,         0},
+	{70,     40,         4,              25,              5,			 5,        20,      10,      16,       0,        1,         0},
+	{80,     40,         5,              35,			  5,			 6,        26,      12,      18,       1,        1,         0},
+	{90,     50,         5,              50,			  5,			 7,        35,      20,      30,       1,        1,         0},
+	{100,    50,         5,              100,			  5,			 8,        45,      35,      40,       1,        1,         0},
+	{110,    60,         5,              200,			  5,			 9,        55,      40,      65,       1,        1,         0},
+	{120,    60,         5,              300,			  5,			 10,       65,      50,      70,       1,        1,         0},
 }; //placeholder values
 
 vector<Item*> Maze::itemList = {};
