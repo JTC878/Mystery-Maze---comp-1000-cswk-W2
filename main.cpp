6e6725ad1845f947eacd7fbcdc0b346b1f88ae05
JTC878 <jords1321@gmail.com>
2025-01-05 19:17:36 +0000

    put the movementChoice method into main

--- main.cpp
+++ main.cpp
@@ -69,7 +69,7 @@ int main() {
 			int enemyStep = Enemy::getEnemyStep();
 			for (int i = 0; i < enemyStep; i++) { //all of this can easily be made into a function inside Maze class with player pos parameter
 				for (Enemy* enemy : maze1.enemyList) {
-					enemy->enemyRandomMove(maze1.mazeArr, maze1.pathArr, player1.getPlayerPos());
+					enemy->movementChoice(maze1.mazeArr, maze1.pathArr, player1.getPlayerPos(), maze1.pathCount);
 				}
 			}
 		}
